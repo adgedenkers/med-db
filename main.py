6338bf97e4b0040e0f463da735eac6bc935e7b7d
Adge Denkers <adge.denkers@gmail.com>
2022-03-27 10:50:51 -0400

    added csv

---
+++ main.py
@@ -0,0 +1,21 @@
+# Author:  Adge Denkers | https://github.com/adgedenkers
+# Created: Sun Mar 27 2022
+# Updated: 
+# Version: 0.1
+# File:    main.py
+# -----------------------------------------------------------------------------
+
+# imports
+import datetime
+import pandas as pd
+import numpy as np
+import pyto_ui as ui
+
+# main function
+def main():
+    
+    now = datetime.now()
+    now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+
+if __name__ == "__main__":
+    main()
