8b8894b4327fd4f09eef39a96afa9b8cf02c1bb0
Adge Denkers <adge.denkers@gmail.com>
2022-03-27 10:53:55 -0400

    updated main

--- main.py
+++ main.py
@@ -9,13 +9,15 @@
 import datetime
 import pandas as pd
 import numpy as np
-import pyto_ui as ui
+#import pyto_ui as ui
 
 # main function
 def main():
-    
-    now = datetime.now()
+
+    now = datetime.datetime.now()
     now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+    df = pd.read_csv("medications.csv")
+    print(df)
 
 if __name__ == "__main__":
     main()
